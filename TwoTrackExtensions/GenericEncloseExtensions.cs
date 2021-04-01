52f984e529d799efa43e7d62b2bee0df9750f525
AndersAndreen <anders@re-kreation.se>
2021-04-01 10:51:52 +0200

    Add more Enclose extensions

--- TwoTrackExtensions/GenericEncloseExtensions.cs
+++ TwoTrackExtensions/GenericEncloseExtensions.cs
@@ -20,9 +20,19 @@ namespace TwoTrackExtensions
             return result1.Select(value1 => (value1, @delegate(value1)));
         }
 
-        public static ITwoTrack<(T, T2)> Enclose<T, T2>(this ITwoTrack<T> result1, Func<T, ITwoTrack<T2>> @delegate)
+        public static ITwoTrack<(T1, T2)> Enclose<T1, T2>(this ITwoTrack<T1> result1, Func<T1, ITwoTrack<T2>> @delegate)
         {
             return result1.Select(value1 => @delegate(value1).Select(value2 => (value1, value2)));
+        }
+
+        public static ITwoTrack<(T1, T2, T3)> Enclose<T1, T2, T3>(this ITwoTrack<(T1, T2)> result1, Func<(T1, T2), T3> @delegate)
+        {
+            return result1.Select(value1 => (value1.Item1, value1.Item2, @delegate(value1)));
+        }
+
+        public static ITwoTrack<(T1, T2, T3)> Enclose<T1, T2, T3>(this ITwoTrack<(T1, T2)> result1, Func<(T1,T2), ITwoTrack<T3>> @delegate)
+        {
+            return result1.Select(value1 => @delegate(value1).Select(value2 => (value1.Item1, value1.Item2, value2)));
         }
     }
 }
