c3c4b86ecdb6d4f0352a84c127e26f1ad1ba476b
AndersAndreen <anders@re-kreation.se>
2021-04-04 09:42:45 +0200

    Add documentation comments for TtError

--- TwoTrackCore/TtError.cs
+++ TwoTrackCore/TtError.cs
@@ -6,9 +6,24 @@ namespace TwoTrackCore
 {
     public class TtError : ValueObject<TtError>
     {
+        /// <summary>
+        /// ErrorLevel defines the severity of the error. See detailed description of the different levels at <see cref="ErrorLevel"/>.
+        /// </summary>
         public ErrorLevel Level { get; private set; } = ErrorLevel.Error;
+        /// <summary>
+        /// Category is a genesal identifier that can be used for many differend purpouses: 
+        /// When logging it can be useed to group errors.
+        /// When sending messages to a view (via ModelState) it can hold the ID-reference that Modelstate uses.
+        /// When used in a webb API it can be used to hold HTTP-response codes.
+        /// </summary>
         public string Category { get; private set; }
+
+        /// <summary>
+        /// Description may hold a resource key or an error message in a MVC-app, an internal error code when logging
+        /// or an API-related error code when used in an API.
+        /// </summary>
         public string Description { get; private set; }
+
         public string StackTrace { get; private set; }
 
         private TtError()
